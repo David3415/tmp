41598af1815ef37d93bac78b3f25ceb5a67f8e9b
David <Dgagiev@gmail.com>
2024-05-15 17:06:32 +0300

    temp

--- app/src/main/java/com/example/tmpapp1/MainActivity.kt
+++ app/src/main/java/com/example/tmpapp1/MainActivity.kt
@@ -4,21 +4,40 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tmpapp1.databinding.ActivityMainBinding
+
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    val lostArray = arrayOf(10000, 2300)
-    val earnArray = arrayOf(15000, 300)
-    val resultArray = ArrayList<String>()
+    lateinit var binding: ActivityMainBinding
+    val bad = 0..3
+    val normal = 4..6
+    val nice = 7..9
+    val excellent = 10
+
+    val gradeArray = arrayOf(4, 7, 3, 6, 10, 2)
+    val nameArray = arrayOf("Антон", "Егор", "Маша", "Светлана", "Юля", "Семен")
+
+    val badArray = ArrayList<String>()
+    val normalArray = ArrayList<String>()
+    val niceArray = ArrayList<String>()
+    val excellentArray = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //var arrays = resources.getStringArray(R.array.names)
 
-        val names = resources.getStringArray(R.array.names)
-        for ((index, name) in names.withIndex()) {
-            resultArray.add("Имя: $name - прибыль = ${earnArray[index] - lostArray[index]}")
-            Log.d("MyLog", "Статистика -/- ${resultArray[index]}")
+
+        // val range2 = 'a'..'z'
+        for (c in bad)badArray.add(nameArray[c])
+        for (c in normal)normalArray.add(normalArray[c])
+
+            Log.d("MyLog", "Плохие оценки: Ученик: -  ${badArray[c]}")
         }
+
     }
 }
+
+
