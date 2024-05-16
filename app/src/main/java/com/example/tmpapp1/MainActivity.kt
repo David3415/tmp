06330c3e99f293ad74ce169456dc184a7fb8f326
David <Dgagiev@gmail.com>
2024-05-16 17:27:18 +0300

    Pifagor полностью готов

--- app/src/main/java/com/example/tmpapp1/MainActivity.kt
+++ app/src/main/java/com/example/tmpapp1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.tmpapp1
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tmpapp1.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     val lostArray = arrayOf(10000, 2300)
     val earnArray = arrayOf(15000, 300)
     val resultArray = ArrayList<String>()
@@ -13,8 +18,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
+    fun onClickResult(view: View) {
+        if (!isBoolEmpty()){
+            val res=getString(R.string.result_info)+getResult()
+            binding.tvResult.text=res
+        }
+    }
 
+    private fun isBoolEmpty(): Boolean {
+        binding.apply {
+            if (edA.text.isNullOrEmpty()) edA.error = "Поле должно быть заполненно"
+            if (edB.text.isNullOrEmpty()) edB.error = "Поле должно быть заполненно"
+            return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
+    }
+
+    private fun getResult(): String {
+        val a: Double
+        val b: Double
+        binding.apply {
+            a = edA.text.toString().toDouble()
+            b = edB.text.toString().toDouble()
+        }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
     }
 }
+
